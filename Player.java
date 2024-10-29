c0de86320fd74b83e54e58624ee9d03ae3f79d7c
s245742 <s245742@dtu.dk>
2024-10-29 15:51:44 +0100

    Update Player.java
    player class updated with constructor and methods

--- Player.java
+++ Player.java
@@ -1,1 +1,27 @@
+class Player {
+    private String name;
+    private Account account;
+    private int startBalance = 1000;
 
+    public Player(String name) {
+        this.name = name;
+        this.account.setBalance(startBalance);
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Account getAccount() {
+        return this.account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+}
