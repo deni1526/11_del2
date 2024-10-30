09c138cb82ff619c93dfa41efb0d36bd8eae4ce9
YamatoCruiser <peter.hulthin.rasmussen@gmail.com>
2024-10-30 14:51:49 +0100

    Test (3) skellet

--- Test.java
+++ Test.java
@@ -1,31 +1,42 @@
 class test {
     public static void main(String[] args) {
-        var d1 = new Die(1, 6);
-        var d2 = new Die(1, 6);
-        var a1 = new Account(1000)
+        var cup = new RaffleCup(2);
+        cup.createDie(2);
+        var acc = new Account(1000);
 
         //General cash growth test
-        for (int i; i < 100; i++) {
-            Rafflecup.shakeRaffle(d1, d2);
-            aaa.getDiceSum(d1, d2);
+        for (int i = 0; i < 100; i++) {
+            cup.shakeRaffle();
+            cup.getSum();//placeholder
             //land paa felt og koer effekt (werwall ekstra tur ignoreres)
             //opdater account
             if (i==10 || i==20 || i==30 || i==40 || i==50 || i==60 || i==70 || i==80 || i==90 || i==100) {
-                System.out.println(a1.balance);
+                System.out.println("Balance amount after " + i + " turns: " + acc.balanceAmount);//acc.balanceAmount(placeholder)
             }
-
         }
+        //No negative test
+        int neg = 0;
+        for (int i = 0; i < 1000; i++) {
+            cup.shakeRaffle();
+            cup.getSum();//placeholder
+            //land paa felt og koer effekt (werwall ekstra tur ignoreres)
+            //opdater account
+            if (acc.balanceAmount < 0) neg++; //acc.balanceAmount(placeholder)
+            if (i == 1000) System.out.println("Number of times balance was negative during " + i + " turns: " + neg);
+        }
+        
         //Speed test (1 turn)
         long begin = System.nanoTime();
 
-        //Her skal en tur spilles ud
-        RaffleCup.shakeRaffle(d1, d2);
-        aaa.getDiceSum(d1, d2);
+        cup.shakeRaffle();
+        cup.getSum();
+        //land paa felt og koer effekt (werwall ekstra tur ignoreres)
+        //opdater account
 
         long end = System.nanoTime();
-        long timeNano = finish - start;
+        long timeNano = begin - end;
         double timeMilli = timeNano/1e6;
 
-        System.out.println(timeMilli);
+        System.out.println("time for one turn to play out is: " + timeMilli + "ms");
     }
 }
