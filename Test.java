469bd5d2821c910dc8ac779ae8965b47be9cbc4d
YamatoCruiser <peter.hulthin.rasmussen@gmail.com>
2024-10-30 17:00:43 +0100

    Test temp update

--- Test.java
+++ Test.java
@@ -1,37 +1,36 @@
 class test {
     public static void main(String[] args) {
-        var cup = new RaffleCup();
-        cup.createDie(2);
+        var cup = new RaffleCup(2);
         var acc = new Account(1000);
 
         //General cash growth test
         for (int i = 0; i < 100; i++) {
-            cup.shakeRaffle();
-            cup.getSum();//placeholder
-            //land paa felt og koer effekt (werwall ekstra tur ignoreres)
-            //opdater account
+            cup.shakeDie();
+            cup.getDieSum();
+            Field.getFieldValue();
+            Player.getAccountBalance();
             if (i==10 || i==20 || i==30 || i==40 || i==50 || i==60 || i==70 || i==80 || i==90 || i==100) {
-                System.out.println("Balance amount after " + i + " turns: " + acc.balanceAmount);//acc.balanceAmount(placeholder)
+                System.out.println("Balance amount after " + i + " turns: " + Player.getAccountBalance());
             }
         }
         //No negative test
         int neg = 0;
         for (int i = 0; i < 1000; i++) {
-            cup.shakeRaffle();
-            cup.getSum();//placeholder
-            //land paa felt og koer effekt (werwall ekstra tur ignoreres)
-            //opdater account
-            if (acc.balanceAmount < 0) neg++; //acc.balanceAmount(placeholder)
+            cup.shakeDie();
+            cup.getDieSum();
+            Field.getFieldValue();
+            Player.getAccountBalance();
+            if (Player.getAccountBalance() < 0) neg++;
             if (i == 1000) System.out.println("Number of times balance was negative during " + i + " turns: " + neg);
         }
         
         //Speed test (1 turn)
         long begin = System.nanoTime();
 
-        cup.shakeRaffle();
-        cup.getSum();
-        //land paa felt og koer effekt (werwall ekstra tur ignoreres)
-        //opdater account
+        cup.shakeDie();
+        cup.getDieSum();
+        Field.getFieldValue();
+        Player.getAccountBalance();
 
         long end = System.nanoTime();
         long timeNano = begin - end;
