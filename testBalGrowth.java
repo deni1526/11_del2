9f8b1b48c74aa9d96d861b681dbb8ceeed3e591d
YamatoCruiser <peter.hulthin.rasmussen@gmail.com>
2024-10-31 11:33:02 +0100

    Test opdatering
    test opdateret og adskilt

---
+++ testBalGrowth.java
@@ -0,0 +1,17 @@
+public class testBalGrowth {
+    public static void main(String[] args) {
+        var cup = new RaffleCup(2, 1, 6);
+        var field = new Field();
+        var player = new Player[1];
+        player[0] = new Player("test");
+
+        //General cash growth test
+        for (int i = 0; i < 100; i++) {
+            cup.shakeDie();
+            player[0].updateAccountBalance(field.getFieldValue(cup.getDieSum()));
+            if (i==10 || i==20 || i==30 || i==40 || i==50 || i==60 || i==70 || i==80 || i==90 || i==100) {
+                System.out.println("Balance amount after " + i + " turns: " + player[0].getAccountBalance());
+            }
+        }
+    }
+}
---
+++ testSpeed.java
@@ -0,0 +1,20 @@
+public class testSpeed {
+    public static void main(String[] args) {
+        var cup = new RaffleCup(2, 1, 6);
+        var field = new Field();
+        var player = new Player[1];
+        player[0] = new Player("test");
+
+        //Speed test (1 turn)
+        long begin = System.nanoTime();
+
+        cup.shakeDie();
+        player[0].updateAccountBalance(field.getFieldValue(cup.getDieSum()));
+
+        long end = System.nanoTime();
+        long timeNano = begin - end;
+        double timeMilli = timeNano/1e6;
+
+        System.out.println("time for one turn to play out is: " + timeMilli + "ms");
+    }
+}
